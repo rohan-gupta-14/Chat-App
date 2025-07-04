69038d9286dc8eda6eb511986462f133650454b8
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-04 15:54:53 +0530

    Route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>Hello Ravi</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/> 
+        <Route path='/login' element={<LoginPage/>}/> 
+        <Route path='/profile' element={<ProfilePage/>}/> 
+      </Routes>
+    </div>
   )
 }
 
