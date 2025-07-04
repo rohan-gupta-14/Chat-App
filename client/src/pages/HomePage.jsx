f10fa8b090dd8c8c2fd632db892dfa11df15e5ad
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-04 16:03:52 +0530

    home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+
+    </div>
   )
 }
 
