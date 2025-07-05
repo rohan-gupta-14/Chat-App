1b9dbc68fe1b665a40115602d6383d800a97694b
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-05 14:35:39 +0530

    Added grid layout

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import LeftSidebar from '../components/RightSidebar'
 
 const HomePage = () => {
+
+  
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-      hello
+      <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative'>
+        <Sidebar/>
+        <ChatContainer/>
+        <RightSidebar/>
+      </div>
     </div>
   )
 }
