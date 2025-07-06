9bfaa0ac0d6e872e9f25db67bac614b826257670
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-06 16:38:38 +0530

    Chat box time

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utilis';
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
+
+    const scrollEnd = useRef()
+
+     useEffect(()=>{
+        if(scrollEnd.current){
+            scrollEnd.current.scrollIntoView({behavior: "smooth"})
+        }
+     },[])
+
   return selectedUser ? (
     <div className='h-full overflow-scroll relative  backdrop-blur-lg'>
       {/*Header */}
@@ -26,11 +36,13 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
             )}
             <div className='text-center text-xs'>
               <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full' />
-              <p className='text-gray-500'>{msg.createdAt}</p>
+              <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
             </div>
 
           </div>
         ))}
+        <div ref={scrollEnd}></div>
+
       </div>
     </div>
   ) : (
