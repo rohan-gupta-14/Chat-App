14e268b3525410d0d70492ff2fc6db008ce018c0
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-08 16:56:05 +0530

    Right side bar

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-const RightSidebar = () => {
-  return (
-    <div>RightSidebar</div>
+
+const RightSidebar = ({selectedUser}) => {
+  return selectedUser && (
+    <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-mid:hidden" : ""}`}>
+      <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full' />
+        <h1 className='px-10 text-xl font-medium mx-auto flex itmens-center gap-2'>
+          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+          {selectedUser.fullName}
+        </h1>
+        <p className='px-10 mx-auto'>{selectedUser.bio}</p>
+      </div>
+    </div>
   )
 }
 
