9e38d005d8d2b5d2ace96c7b5ae328e250c557e5
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-10 14:35:29 +0530

    all the usestate for login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,7 +4,11 @@ import assets from '../assets/assets'
 const LoginPage = () => {
 
   const [currState, setCurrSate] = useState("Sign up")
-
+  const [fullName, setfullName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [bio, setBio] = useState("")
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false)
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
 
@@ -18,7 +22,9 @@ const LoginPage = () => {
         {currState}
         <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
       </h2>
-      
+      {currState === "Sign up" && (
+        <input type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required />
+      )}
       </form>
 
     </div>
