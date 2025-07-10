7f4e36bf031d2a31d57714d9cebb18eaa7206aa8
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-10 14:01:23 +0530

    media

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { imagesDummyData } from '../assets/assets'
 
 
 const RightSidebar = ({selectedUser}) => {
@@ -7,12 +7,30 @@ const RightSidebar = ({selectedUser}) => {
     <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-mid:hidden" : ""}`}>
       <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full' />
-        <h1 className='px-10 text-xl font-medium mx-auto flex itmens-center gap-2'>
+        <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
           <p className='w-2 h-2 rounded-full bg-green-500'></p>
           {selectedUser.fullName}
         </h1>
         <p className='px-10 mx-auto'>{selectedUser.bio}</p>
       </div>
+
+      <hr className='border-[#ffffff50] my-4'/>
+
+      <div className='px-5 text-xs'>
+        <p>Media</p>
+        <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
+          {imagesDummyData.map((url,index)=>(
+            <div key={index} onClick={()=> window.open(url)} className='cursor-pointer rounded'>
+              <img src={url} alt="" className='h-full rounded-md' />
+
+            </div>
+
+          ))}
+        </div>
+      </div>
+
+      <button></button>
+
     </div>
   )
 }
