c6791d3ca00453bdf54e2476639191d3b82a5906
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-12 15:07:59 +0530

    server setup

--- server/server.js
+++ server/server.js
@@ -10,5 +10,7 @@ app.use(cors());
 
 app.use("/api/status", (req, res)=> res.send("Server is live"));
 
+
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
