7ef0186c6d60ea2dcdae920f9fc11b1f27d34c8a
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-14 16:38:44 +0530

    middle ware setup completed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,8 +7,12 @@ import User from "../models/User";
     try {
         const token = req.headers.token;
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findById()
+        const user = await User.findById(decoded.userId).select("-password"); 
+        if(!user) return res.json({success: false, message: "User not found"});
+        req.user = user;
+        next();
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({ success: false, message:error.message});
     }
  }
