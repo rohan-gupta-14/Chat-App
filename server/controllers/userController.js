a60e596781cd877a7a2dcd39a9651d709599f204
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-14 16:22:10 +0530

    log in completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -29,3 +29,22 @@ export const signup = async (req,res)=>{
     }
 }
 
+export const login = async (req,res)=>{
+    try {
+        const {email, password} = req.body;
+        const userData = await User.findOne({email})
+
+        const isPasswordCorrect = await bcrypt.compare(process, userData.password);
+
+        if (!isPasswordCorrect){
+            return res.json({success: false, message: "Invalid credentials"});
+        }
+
+        const token = generateToken(newUser._id)
+        res.json({success: true, userData, token, message: "Login successfully"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false,  message: error.message})
+    }
+}
