8b0f6afeb60afe0e1645b59c6f9746aeea947b90
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-14 16:32:26 +0530

    middle ware setup

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,13 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
  
 
  export const protectRoute = async (req, res, next)=>{
     try {
-        
+        const token = req.headers.token;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findById()
     } catch (error) {
         
     }
